ee8c4f8db84f77a22c3b7f5f35a237d3c10678d1
reason <reasonz@daum.net>
2022-06-15 21:47:58 +0900

    REFACTOR product search

--- web-dev/main.js
+++ web-dev/main.js
@@ -1,75 +1,66 @@
-const inputText = document.querySelector('#text'); // 검색창
+const searchInput = document.querySelector('#search-input'); // 검색창
 const productContainer = document.querySelector('.product-container'); // 상품 컨테이너
 const shoppingCart = document.querySelector('.dragbox'); // 드래그 할곳
+let productList = []; // store.json 파일 리스트
 
-/* JSON 파일 동적으로 HTML에 넣기 */
+/* JSON데이터 받아와서 productList 배열에 원본 데이터 저장하기 */
+roadData();
 
-makeProduct();
-
-function makeProduct() {
+/* JSON 데이터 받아오기 */
+function roadData() {
   fetch('./store.json')
     .then((res) => {
       return res.json();
     })
     .then((data) => {
       data.products.forEach((item) => {
-        productContainer.insertAdjacentHTML('beforeend', template(item.title, item.brand, item.photo, item.price));
+        productList.push(item);
       });
+    })
+    .then(() => {
+      makeProduct(productList);
     })
     .catch((error) => {
       console.log('에러');
     });
 }
 
-function template(productName, brandName, photo, price) {
-  return `
-  <div class="product" draggable="true">
-    <img src="img/${photo}" alt="" draggable="false"/>
-    <h2 class="title">${productName}</h2>
-    <p class="brand-name">${brandName}</p>
-    <h3>가격 : ${price}</h3>
-    <button>담기</button>
-  </div>
-`;
+/* 받아온 JSON 데이터 보여주기 */
+function makeProduct(productList) {
+  productList.forEach((product) => {
+    const { title, brand, photo, price } = product;
+    productContainer.insertAdjacentHTML('beforeend', template(title, brand, photo, price));
+  });
 }
 
-inputText.addEventListener('input', function () {
-  const searchTitle = document.querySelectorAll('.title');
-  const searchBrand = document.querySelectorAll('.brand-name');
-  let text = inputText.value.trim(); // 검색어 공백 제거해서 저장
+/* 제품 템플릿 */
+function template(productName, brandName, photo, price) {
+  return `
+    <div class="product" draggable="true">
+      <img src="img/${photo}" alt="" draggable="false"/>
+      <h2 class="title">${productName}</h2>
+      <p class="brand-name">${brandName}</p>
+      <h3>가격 : ${price}</h3>
+      <button>담기</button>
+    </div>
+  `;
+}
 
-  searchTitle.forEach((title) => {
-    title.parentElement.style.visibility = 'hidden';
-    title;
-    if (title.innerHTML.indexOf(text) !== -1 && text !== '') {
-      // 포함된 상품만 보여주기
-      title.parentElement.style.visibility = 'visible';
-      productContainer.prepend(title.parentElement); // 부모의 첫번째 자식으로 해당 상품 추가
-    }
+searchInput.addEventListener('keyup', function () {
+  let inputText = searchInput.value.trim().toUpperCase(); // 검색어 공백 제거해서 저장
+  const productList = newProductList(inputText);
 
-    if (text === '') {
-      setTimeout(() => {
-        title.parentElement.style.visibility = 'visible';
-      }, 100);
-    }
-  });
-
-  // 브랜드 검색시
-  searchBrand.forEach((brand) => {
-    if (brand.innerHTML.indexOf(text) !== -1 && text !== '') {
-      let brandArr = [];
-      brandArr.push(brand.parentElement);
-
-      brand.parentElement.style.visibility = 'visible';
-      productContainer.prepend(brand.parentElement);
-    }
-  });
-
-  if (text === '') {
-    productContainer.innerHTML = '';
-    makeProduct(); // 검색어 있는 상품이 검색되면 위치 변경되므로 상품 재배치
-  }
+  productContainer.innerHTML = '';
+  makeProduct(productList);
 });
+
+/* 검색에 해당하는 제품 이름/브랜드를 담은 아이템 배열 만들기 */
+function newProductList(inputText) {
+  const findProduct = productList.filter((product) => {
+    return product.title.includes(inputText) || product.brand.includes(inputText);
+  });
+  return findProduct;
+}
 
 // 드래그 & 드랍 이벤트
 productContainer.addEventListener('dragstart', (e) => {
@@ -96,6 +87,3 @@ shoppingCart.addEventListener('dragover', (e) => {
     }
   });
 });
-
-// 드래그 후 장바구니에 옮겨지긴 하는데
-// 드래그하기 전 아이템들이 사라지면 안됨.
